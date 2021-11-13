03c8c979befa8201cd5ebcea3eb697a1db2aaf9a
khaoula <khaoulajabour4@gmail.com>
2021-11-13 02:06:06 -0500

    implement drag-drop functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,6 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
-
-
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
@@ -133,4 +131,62 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper:"clone",
+  activate: function (event) {
+    console.log("activate",this);
+  },
+  deactivate: function (event) {
+    console.log("deactivate",this);
+  },
+  over: function (event) {
+    console.log("over",event.target);
+  },
+  out: function (event) {
+    console.log("out",event.target);
+  },
+  update: function (event) {
+    var tempArr = [];
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
 
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+  
+});
+var arrName = $(this).attr("id").replace("list-","");
+  tasks[arrName]= tempArr;
+  saveTasks();
+console.log(tempArr);
+  }
+});
+
+
+$("#trash").droppable({
+accept:".card .list-group-item",
+tolerance: "touch",
+drop:function (event,ui) {
+  ui.draggable.remove();
+},
+over: function (event) {
+  console.log("over");
+},
+out:function (event) {
+  console.log("out");
+}
+})
