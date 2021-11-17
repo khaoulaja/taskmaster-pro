5d41b548d726971eb40af74f3cff22b86e985d76
khaoula <khaoulajabour4@gmail.com>
2021-11-17 02:33:07 -0500

    custom styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,7 +129,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -167,27 +167,41 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper:"clone",
+  activate: function (event, ui ) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function (event, ui ) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function (event, ui ) {
+    $(event.target).addClass("dropover-active");
+  },
+  out: function (event, ui ) {
+    $(event.target).removeClass("dropover-active");
+  },
   update: function (event) {
     var tempArr = [];
-// loop over current set of children in sortable list
-$(this).children().each(function() {
-  var text = $(this)
-    .find("p")
-    .text()
-    .trim();
+  // loop over current set of children in sortable list
+  $(this).children().each(function() {
+    var text = $(this)
+      .find("p")
+      .text()
+      .trim();
 
-  var date = $(this)
-    .find("span")
-    .text()
-    .trim();
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
 
-  // add task data to the temp array as an object
-  tempArr.push({
-    text: text,
-    date: date
-  });
-  
+    // add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    }); 
 });
+
 var arrName = $(this).attr("id").replace("list-","");
   tasks[arrName]= tempArr;
   saveTasks();
@@ -201,6 +215,13 @@ accept:".card .list-group-item",
 tolerance: "touch",
 drop:function (event,ui) {
   ui.draggable.remove();
+  $(".bottom-trash").removeClass("bottom-trash-active");
+},
+over: function (event, ui ) {
+  $(".bottom-trash").addClass("bottom-trash-active");
+},
+out: function (event, ui ) {
+  $(".bottom-trash").removeClass("bottom-trash-active");
 }
 });
 
